291bb666cc88c7834a148dd9f5a53b8cb3133d7f
danvasquesc <dan.vasques@outlook.com.br>
2026-03-20 15:36:35 -0300

    feat: Redeclaração de Array

--- src/NivelBasico/Array.java
+++ src/NivelBasico/Array.java
@@ -18,6 +18,11 @@ public class Array {
         ninja[2] = "Sakura";
         ninja[3] = "Hinata";
 
+        //Como mostrar os nomes do array
+        for (int i = 0; i < ninja.length ; i++) {
+            System.out.println(ninja[i]);
+        }
+
         System.out.println(ninja);
         /*
          * Se é impresso 'System.out.println(ninja);', ou seja, imprimir o Array, irá aparecer @76ed5528, pois é a referencia na nossa memoria de onde está armazenado o array
@@ -28,5 +33,25 @@ public class Array {
         //Array do tipo int, se nao colocar valor nenhum, retorna 0
         //Array do tipo boolean, se nao colocar valor nenhum, retorna false
         //Array do tipo double, se nao colocar valor nenhum, retorna 0.0
+
+        //Redeclarar um array
+        int[] idade = new int[6];
+        idade[0] = 13;
+        idade[1] = 14;
+        idade[2] = 15;
+        idade[3] = 16;
+
+        System.out.println("Localizador da memoria antigo: " + idade);
+
+        idade = new int[6];
+        //aqui podem ser adicionados novos elementos normalmente
+        idade[0] = 20;
+        System.out.println("Localizador da memoria atual: " + idade);
+        System.out.println(idade[0]);
+
+        /*
+        * O array antigo não existira mais pois foi redeclarado, ou seja, sobescrito, e ai surgirá mais pra frente o Garbage Collector para jogar fora o antigo array.
+        * */
+
     }
 }
