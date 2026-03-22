7422decf93aa718801df0207aeee7b473e8126dd
danvasquesc <dan.vasques@outlook.com.br>
2026-03-22 16:25:23 -0300

    feat: Classes e Objetos

---
+++ src/NivelIntermediario/Aula01/Main.java
@@ -0,0 +1,21 @@
+package NivelIntermediario.Aula01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Criar ninjas com objetos
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.aldeia = "Aldeia da Folha";
+        Naruto.idade = 17;
+
+        //Criar ninja Sasuke - ele é um objeto
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.aldeia = "Aldeia da Folha";
+        Sasuke.idade = 18;
+
+        //Naruto e Sasuke são objetos
+
+    }
+}
---
+++ src/NivelIntermediario/Aula01/Ninja.java
@@ -0,0 +1,10 @@
+package NivelIntermediario.Aula01;
+
+public class Ninja {
+
+    //Molde para os ninjas
+    String nome;
+    String aldeia;
+    int idade;
+
+}
